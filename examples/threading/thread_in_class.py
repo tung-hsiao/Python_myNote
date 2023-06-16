c9689cf370ff4abf867e80a85bc77f523314b051
tung-hsiao <86992241+tung-hsiao@users.noreply.github.com>
2023-06-16 11:23:55 +0800

    Update thread_in_class.py

--- examples/threading/thread_in_class.py
+++ examples/threading/thread_in_class.py
@@ -5,15 +5,19 @@ class myClass(object):
     def __init__(self, logger):
         self.event_queue = []
         self.logger = logger
+        self.running = True
 
     def run(self):
       t = threading.Thread(target=self.process, args=())
       t.setDaemon(True)
       t.start()
-
+    
+    def stop(self):
+        self.running = False
+    
     def process(self):
         self.logger.info(f"[ process ] Start! ")
-        while True:
+        while self.running:
             print("Zz..")
             time.sleep(1)
 
